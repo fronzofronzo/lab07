5b04a73c57e9145521cc2f5a9b607ed0299a79ef
Gabriele Fronzoni <gabriele.fronzoni@studio.unibo.it>
2023-11-09 17:46:41 +0100

    ciao

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -9,6 +9,56 @@ import java.util.Objects;
  */
 public final class MonthSorterNested implements MonthSorter {
 
+    public enum Month {
+        JANUARY("January",31),
+        FEBRUARY("February",28),
+        MARCH("March",31),
+        APRIL("April",30),
+        MAY("May",31),
+        JUNE("June",30),
+        JULY("July",31),
+        AUGUST("August",30),
+        SEPTEMBER("September",30),
+        OCTOBER("October",31),
+        NOVEMBER("November", 30),
+        DECEMBER("December",31);
+
+        private final String name;
+        private final int numDays;
+
+        private Month(final String name, final int actualNumOfDays) {
+            this.name = name;
+            this.numDays = actualNumOfDays;
+        }
+
+        public static Month fromString(String text) {
+            int corrispondences = 0;
+            Month matchMonth = null; 
+            for(Month m : Month.values()) {
+                final String toUpper = m.name.toUpperCase();
+                final String toLower = m.name.toLowerCase();
+                    if(text.length() <= m.name.length()) {
+                        if(text.compareTo(m.name.substring(0,text.length())) == 0 || text.compareTo(toUpper.substring(0,text.length()) )==
+                        0 || text.compareTo(toLower.substring(0,text.length())) == 0) {
+                            corrispondences++;
+                            matchMonth = m;
+                        }
+                    }
+            }
+            if(corrispondences == 0) {
+                throw new IllegalArgumentException("No Month with such name");
+            }
+            if(corrispondences > 1) {
+                throw new IllegalArgumentException("Ambigous Input");
+            }
+            return matchMonth;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
     @Override
     public Comparator<String> sortByDays() {
         return null;
